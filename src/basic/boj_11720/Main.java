9059e2b0815841d7bd259e42232bf662f398ca9a
DongWon Lee <atropic159@hanyang.ac.kr>
2026-01-23 20:57:30 +0900

    solve: [BOJ] 11720 숫자의 합

---
+++ src/basic/boj_11720/Main.java
@@ -0,0 +1,24 @@
+package basic.boj_11720;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String input = br.readLine();
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += input.charAt(i) - '0';
+        }
+        /*
+            // for-each 활용
+            for (char c : input.toCharArray()) {
+                sum += Character.getNumericValue(c);
+            }
+         */
+
+        System.out.print(sum);
+    }
+}
