be89fc4b07ca88697402027f48f2a778325135fc
DongWon Lee <atropic159@hanyang.ac.kr>
2026-01-24 15:17:15 +0900

    solve: [BOJ] 14425 문자열 집합

---
+++ src/basic/boj_14425/Main.java
@@ -0,0 +1,28 @@
+package basic.boj_14425;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        HashSet<String> sSet = new HashSet<>((int) (N / 0.75 + 1));
+        for(int i = 0; i < N; i++) {
+            sSet.add(br.readLine());
+        }
+
+        int count = 0;
+        for(int i = 0; i < M; i++) {
+            String input = br.readLine();
+            if(sSet.contains(input)) {
+                count++;
+            }
+        }
+
+        System.out.print(count);
+    }
+}
