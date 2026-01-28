36e183ace58fbe512322b9b1e662fbed6d53d1ba
DongWon Lee <atropic159@hanyang.ac.kr>
2026-01-28 18:45:15 +0900

    solve: [BOJ] 2563 색종이

---
+++ src/basic/boj_2563/Main.java
@@ -0,0 +1,31 @@
+package basic.boj_2563;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[][] paper = new int[100][100];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int count = 0;
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            for(int y = b; y < b + 10; y++) {
+                for(int x = a; x < a + 10; x++) {
+                    if(paper[y][x] == 0) {
+                        paper[y][x] = 1;
+                        count++;
+                    }
+                }
+            }
+        }
+
+        System.out.print(count);
+    }
+}
