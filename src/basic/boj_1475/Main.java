512e3c147c6f5595571afc5ef4e2358c40d6f691
DongWon Lee <atropic159@hanyang.ac.kr>
2026-01-31 02:00:45 +0900

    solve: [BOJ] 1475 방 번호

---
+++ src/basic/boj_1475/Main.java
@@ -0,0 +1,36 @@
+package basic.boj_1475;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String N = br.readLine();
+        int[] numArr = new int[10];
+
+        for(int i = 0; i < N.length(); i++){
+            int num = N.charAt(i) - '0';
+
+            if(num == 9) {
+                numArr[6]++;
+                continue;
+            }
+            numArr[num]++;
+        }
+
+        if(numArr[6] % 2 == 1) {
+            numArr[6] = numArr[6] / 2 + 1;
+        } else {
+            numArr[6] /= 2;
+        }
+
+        int count = 0;
+        for(int i : numArr) {
+            count = Math.max(i, count);
+        }
+
+        System.out.println(count);
+    }
+}
