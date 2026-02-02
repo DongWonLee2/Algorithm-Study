b815dc5288f72df06d43979a6ffe6f74e3a5cecf
DongWon Lee <atropic159@hanyang.ac.kr>
2026-02-02 16:45:00 +0900

    solve: [BOJ] 1193 분수찾기

---
+++ src/basic/boj_1193/Main.java
@@ -0,0 +1,38 @@
+package basic.boj_1193;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+
+        if(X == 1) {
+            System.out.print("1/1");
+            return;
+        }
+
+        int count = 0;
+        int sum = 2;
+        int n = 1;
+        for(int i = 1; i <= X; i++) {
+            if(count == n) {
+                sum++;
+                n++;
+                count = 0;
+            }
+            count++;
+        }
+
+        if(sum % 2 == 0) {
+            int a = sum - count;
+            int b = count;
+            System.out.print(a + "/" + b);
+        } else {
+            int a = count;
+            int b = sum - count;
+            System.out.print(a + "/" + b);
+        }
+    }
+}
