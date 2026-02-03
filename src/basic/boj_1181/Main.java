7ab58a7d0b3b94b122fb1804c16f4654bff8670a
DongWon Lee <atropic159@hanyang.ac.kr>
2026-02-03 17:25:54 +0900

    solve: [BOJ] 1181 단어 정렬

---
+++ src/basic/boj_1181/Main.java
@@ -0,0 +1,28 @@
+package basic.boj_1181;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<String> words = new ArrayList<>();
+
+        for(int i = 0; i < N; i++) {
+            String input = br.readLine();
+            if(!words.contains(input)) words.add(input);
+        }
+
+        words.sort(Comparator
+                .comparingInt(String::length)
+                .thenComparing(s -> s));
+
+        StringBuilder sb = new StringBuilder();
+        for(String s : words) {
+            sb.append(s).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
