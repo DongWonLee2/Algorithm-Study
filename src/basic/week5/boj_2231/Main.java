7d47b85c4e7e9722831f7012fa9b0518f039fbde
DongWon Lee <atropic159@hanyang.ac.kr>
2026-02-19 20:25:54 +0900

    solve: [BOJ] 2231 분해합

---
+++ src/basic/week5/boj_2231/Main.java
@@ -0,0 +1,25 @@
+package basic.week5.boj_2231;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int result = 0;
+        for(int i = 1; i < n; i++) {
+            int sum = i;
+            int temp = i;
+            while(temp > 0) {
+                sum += temp % 10;
+                temp /= 10;
+            }
+            if(sum == n) {
+                result = i;
+                break;
+            }
+        }
+        System.out.print(result);
+    }
+}
