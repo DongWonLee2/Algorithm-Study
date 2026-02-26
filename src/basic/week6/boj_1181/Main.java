87a19172a78dfdacd3e981beb9322561915fa351
DongWon Lee <atropic159@hanyang.ac.kr>
2026-02-26 21:41:14 +0900

    solve: [boj] 1181 단어 정렬

---
+++ src/basic/week6/boj_1181/Main.java
@@ -0,0 +1,31 @@
+package basic.week6.boj_1181;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(sc.nextLine());
+
+        String[] words = new String[N];
+        for(int i = 0; i < N; i++) {
+            words[i] = sc.nextLine();
+        }
+
+        Arrays.sort(words, Comparator
+                .comparingInt(String::length)
+                .thenComparing(Comparator.naturalOrder())
+        );
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(words[0]).append("\n");
+        for (int i = 1; i < N; i++) {
+            // 중복 제거
+            if (!words[i].equals(words[i - 1])) {
+                sb.append(words[i]).append("\n");
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
